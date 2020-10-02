f1e487f77f32afcb6720f505d2bf684ffe2982b8
Richard Moss <richard.moss@cyotek.com>
2020-10-02 09:15:07 +0100

    Update runtests.cmd
    * Now a) works and b) tests different targets
    * Does not work with .NET core targets yet

--- runtests.cmd
+++ runtests.cmd
@@ -15,19 +15,33 @@ IF "%RESULTDIR%"=="" SET RESULTDIR=testresults
 IF NOT EXIST %RESULTDIR% MKDIR %RESULTDIR%
 
 SET DLLNAME=Cyotek.Data.Nbt.Tests
-SET SRCDIR=src\%DLLNAME%\
+SET SRCDIR=tests\
 SET SLNNAME=Cyotek.Data.Nbt.sln
 
-%NUGETEXE% restore %SLNNAME%
-
-%MSBUILDEXE% %SLNNAME% %cbbuild%
+dotnet build %SLNNAME% --configuration Release
 IF %ERRORLEVEL% NEQ 0 GOTO :testsfailed
 
-%nunitexe% %SRCDIR%bin\release\%DLLNAME%.dll /xml=testresults\%DLLNAME%.xml %nunitargs%
-IF %ERRORLEVEL% NEQ 0 GOTO :testsfailed
+CALL :runtests net35
+CALL :runtests net40
+CALL :runtests net45
+CALL :runtests net452
+CALL :runtests net462
+CALL :runtests net472
+CALL :runtests net48
+REM CALL :runtests netstandard2.0
+REM CALL :runtests netstandard2.1
+REM CALL :runtests netcoreapp2.1
+REM CALL :runtests netcoreapp3.1
+REM CALL :runtests netcoreapp2.2
 
 ENDLOCAL
 
+GOTO :eof
+
+:runtests
+SET TRG=%1
+%nunitexe% %SRCDIR%bin\x86\release\%TRG%\%DLLNAME%.dll /result=testresults\%DLLNAME%.%TRG%.xml %nunitargs%
+IF %ERRORLEVEL% NEQ 0 GOTO :testsfailed
 GOTO :eof
 
 :testsfailed
